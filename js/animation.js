fabbf0ee4b280bd60a4da547e59bb2a56262fb00
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-11-03 18:47:22 +0100

    main site .psd file has been cut. Main section - mobile.

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,13 @@
 $(function () {
 
+let hamburger = $('.hamburger');
 
+function animatedHamburger() {
+    hamburger.on("click", function () {
+        $(this).toggleClass("is-active");
+    });
+ };
+
+
+animatedHamburger();
 });
