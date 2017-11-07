0205c19b798e362887c8e5ccf1f6edb11c4b01de
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-11-07 15:33:55 +0100

    Sticky menu.

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,62 @@
 $(function () {
 
-let hamburger = $('.hamburger');
 
-function animatedHamburger() {
-    hamburger.on("click", function () {
-        $(this).toggleClass("is-active");
+    /// hamburger    
+
+    let hamburger = $('.hamburger');
+
+    function animatedHamburger() {
+        hamburger.on("click", function () {
+            $(this).toggleClass("is-active");
+        });
+    };
+
+
+    /// sticky menu
+
+    const $sticky = $('.stickyBar');
+    const $logo = $('.logo');
+    const $ul = $sticky.find('.menu');
+    const top = $ul.offset().top;
+
+    $(function () {
+        $(window).on('scroll', function () {
+            const pix = $(document).scrollTop();
+            console.log('scroll: ' + pix);
+
+            if (pix > top) {
+                $sticky.addClass('sticky');
+                $logo.addClass('sticky-logo');
+                $ul.addClass('sticky-menu');
+            } else {
+                $sticky.removeClass('sticky');
+                $logo.removeClass('sticky-logo');
+                $ul.removeClass('sticky-menu');
+            }
+        })
     });
- };
+
+    $(window).on('rezise', function () {
+        if ($sticky.hasClass('sticky')) {
+            pix = $ul.offset().top;
+        } else {
+            pix = $sticky.offset().top;
+        }
+
+        const pix = $(document).scrollTop();
+
+        if (pix > top) {
+            $sticky.addClass('sticky');
+            $logo.addClass('sticky-logo');
+            $ul.addClass('sticky-menu');
+        } else {
+            $sticky.removeClass('sticky');
+            $logo.removeClass('sticky-logo');
+            $ul.removeClass('sticky-menu');
+        }
+    });
 
 
-animatedHamburger();
+
+    animatedHamburger();
 });
